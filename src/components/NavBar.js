cb7c96efe7a68f4409b1f2c405ec8b153942bced
Summira Hussain <summira508411@gmail.com>
2022-11-08 14:18:51 +0000

    final push ready to deploy

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -31,7 +31,7 @@ export const NavBar = () => {
       <Navbar expand="lg" className={scrolled ? "scrolled" : ""}>
         <Container>
           <Navbar.Brand href="#home">
-            <img src={logo} alt="logo"/>
+            <img  src={logo} alt="logo" className='home_logo'/>
         </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav">
             <span className='navbar-toggler-icon'> </span>
